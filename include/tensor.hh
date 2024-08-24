851de54ff68c6efa3a4b3f43876d5150222ca150
HeroAdisorn <adisornbkk@gmail.com>
2024-08-24 17:11:04 +0200

    first commit

---
+++ include/tensor.hh
@@ -0,0 +1,92 @@
+#pragma once
+#include <Eigen/Dense>
+#include <vector>
+#include <iterator>
+
+template <typename T>
+class Tensor
+{
+public:
+    Tensor(const std::vector<size_t> &shape) : _shape(shape)
+    {
+        assert(!std::empty(shape));
+        _ndim = _shape.size();
+
+        size_t size = 1;
+        for (size_t i = 0; i < _ndim; i++)
+            size *= _shape[i];
+
+        _size = size;
+        _arr.resize(size);
+    }
+
+    std::vector<T> arr() const
+    {
+        return _arr;
+    }
+
+    std::vector<size_t> shape() const
+    {
+        return _shape;
+    }
+
+    size_t size() const
+    {
+        return _size;
+    }
+
+    size_t ndim() const
+    {
+        return _ndim;
+    }
+
+    void reshape(const std::vector<size_t> &shape)
+    {
+        assert(!std::empty(shape));
+        size_t size = 1;
+
+        for (auto it = shape.begin(); it != shape.end(); ++it)
+        {
+            size *= *it;
+        }
+        assert(size == _size);
+
+        _shape = shape;
+        _ndim = _shape.size();
+    }
+
+    void flatten()
+    {
+        reshape({_size});
+    }
+
+    T &operator[](const std::vector<size_t> &indices)
+    {
+        assert(indices.size() == _ndim);
+
+        size_t index = 0;
+        size_t stride = 1;
+
+        size_t i = 0;
+        for (auto it = indices.rbegin(); it != indices.rend(); ++it)
+        {
+            index += stride * (*it);
+            stride *= _shape[i];
+            ++i;
+        }
+
+        assert(index < _size);
+        return _arr[index];
+    }
+
+    Tensor<T> operator+(Tensor<T> other)
+    {
+        assert(_shape == other.shape());
+    }
+
+private:
+    std::vector<T> _arr;
+    std::vector<size_t> _shape;
+    size_t _ndim;
+    size_t _size;
+};
---
+++ tests/create_tensor.cc
@@ -0,0 +1,24 @@
+#include "tensor.hh"
+#include "Eigen/Dense"
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<size_t> shape = {2, 3, 4};
+    std::vector<size_t> flatten_shape = {24};
+    Tensor<float> t(shape);
+    t[{0, 0, 0}] = 5;
+
+    assert(t.shape() == shape);
+    assert(t.size() == 24);
+    assert(t.ndim() == 3);
+    t.flatten();
+    assert(t.shape() == flatten_shape);
+    assert(t.size() == 24);
+    assert(t.ndim() == 1);
+
+    assert(t[{0}] == 5);
+
+    return 0;
+}
