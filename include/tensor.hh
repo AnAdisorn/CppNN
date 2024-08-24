bd857258dc4e9d8e442ff294f3a0d61e98f48792
HeroAdisorn <adisornbkk@gmail.com>
2024-08-24 19:29:24 +0200

    split to header and implemented files

--- include/tensor.hh
+++ include/tensor.hh
@@ -1,117 +1,35 @@
 #pragma once
-#include <Eigen/Dense>
+#include "Eigen/Dense"
+#include "omp.h"
 #include <vector>
-#include <iterator>
 
 template <typename T>
 class Tensor
 {
 public:
-    Tensor(const std::vector<size_t> &shape) : _shape(shape)
-    {
-        assert(!std::empty(shape));
-        _ndim = _shape.size();
+    // Constructors and destructors
+    Tensor(const std::vector<size_t> &shape);
 
-        size_t size = 1;
-        for (size_t i = 0; i < _ndim; i++)
-            size *= _shape[i];
+    // Accessors
+    const std::vector<T> &arr() const;
+    std::vector<size_t> shape() const;
+    size_t size() const;
+    size_t ndim() const;
 
-        _size = size;
-        _arr.resize(size);
-    }
+    // Modifiers
+    void reshape(const std::vector<size_t> &shape);
+    void flatten();
+    void setArrIndex(size_t index, T val);
 
-    const std::vector<T> &arr() const
-    {
-        return _arr;
-    }
+    // Element access
+    T &operator[](const std::vector<size_t> &indices);
 
-    std::vector<size_t> shape() const
-    {
-        return _shape;
-    }
-
-    size_t size() const
-    {
-        return _size;
-    }
-
-    size_t ndim() const
-    {
-        return _ndim;
-    }
-
-    void reshape(const std::vector<size_t> &shape)
-    {
-        assert(!std::empty(shape));
-        size_t size = 1;
-
-        for (auto it = shape.begin(); it != shape.end(); ++it)
-        {
-            size *= *it;
-        }
-        assert(size == _size);
-
-        _shape = shape;
-        _ndim = _shape.size();
-    }
-
-    void flatten()
-    {
-        reshape({_size});
-    }
-
-    T &operator[](const std::vector<size_t> &indices)
-    {
-        assert(indices.size() == _ndim);
-
-        size_t index = 0;
-        size_t stride = 1;
-
-        size_t i = 0;
-        for (auto it = indices.rbegin(); it != indices.rend(); ++it)
-        {
-            index += stride * (*it);
-            stride *= _shape[i];
-            ++i;
-        }
-
-        assert(index < _size);
-        return _arr[index];
-    }
-
-    void setArrIndex(size_t index, T val)
-    {
-        _arr[i] = val;
-    }
-
-    Tensor<T> operator+(const Tensor<T> &other)
-    {
-        assert(_shape == other.shape());
-        Tensor<T> result({_shape});
-
-        auto &other_arr = other.arr();
-#pragma omp parallel for
-        for (size_t i = 0; i < _size; i++)
-        {
-            result.setArr(i, _arr[i] + other_arr[i]);
-        }
-
-        return result;
-    }
-
-    void operator+=(const Tensor<T> &other)
-    {
-        assert(_shape == other.shape());
-
-        auto &other_arr = other.arr();
-#pragma omp parallel for
-        for (size_t i = 0; i < _size; i++)
-        {
-            _arr[i] += other_arr[i];
-        }
-    }
+    // Arithmetic operations
+    Tensor<T> operator+(const Tensor<T> &other);
+    void operator+=(const Tensor<T> &other);
 
 private:
+    // Private members
     std::vector<T> _arr;
     std::vector<size_t> _shape;
     size_t _ndim;
--- lib/tensor.cc
+++ lib/tensor.cc
@@ -1,1 +1,116 @@
 #include "tensor.hh"
+
+template <typename T>
+Tensor<T>::Tensor(const std::vector<size_t> &shape) : _shape(shape)
+{
+    assert(!std::empty(shape));
+    _ndim = _shape.size();
+
+    size_t size = 1;
+    for (size_t i = 0; i < _ndim; i++)
+        size *= _shape[i];
+
+    _size = size;
+    _arr.resize(size);
+}
+
+template <typename T>
+const std::vector<T> &Tensor<T>::arr() const
+{
+    return _arr;
+}
+
+template <typename T>
+std::vector<size_t> Tensor<T>::shape() const
+{
+    return _shape;
+}
+
+template <typename T>
+size_t Tensor<T>::size() const
+{
+    return _size;
+}
+
+template <typename T>
+size_t Tensor<T>::ndim() const
+{
+    return _ndim;
+}
+
+template <typename T>
+void Tensor<T>::reshape(const std::vector<size_t> &shape)
+{
+    assert(!std::empty(shape));
+    size_t size = 1;
+
+    for (auto it = shape.begin(); it != shape.end(); ++it)
+    {
+        size *= *it;
+    }
+    assert(size == _size);
+
+    _shape = shape;
+    _ndim = _shape.size();
+}
+
+template <typename T>
+void Tensor<T>::flatten()
+{
+    reshape({_size});
+}
+
+template <typename T>
+void Tensor<T>::setArrIndex(size_t index, T val)
+{
+    _arr[index] = val;
+}
+
+template <typename T>
+T &Tensor<T>::operator[](const std::vector<size_t> &indices)
+{
+    assert(indices.size() == _ndim);
+
+    size_t index = 0;
+    size_t stride = 1;
+
+    size_t i = 0;
+    for (auto it = indices.rbegin(); it != indices.rend(); ++it)
+    {
+        index += stride * (*it);
+        stride *= _shape[i];
+        ++i;
+    }
+
+    assert(index < _size);
+    return _arr[index];
+}
+
+template <typename T>
+Tensor<T> Tensor<T>::operator+(const Tensor<T> &other)
+{
+    assert(_shape == other.shape());
+    Tensor<T> result({_shape});
+
+    auto &other_arr = other.arr();
+#pragma omp parallel for
+    for (size_t i = 0; i < _size; i++)
+    {
+        result.setArr(i, _arr[i] + other_arr[i]);
+    }
+
+    return result;
+}
+
+template <typename T>
+void Tensor<T>::operator+=(const Tensor<T> &other)
+{
+    assert(_shape == other.shape());
+
+    auto &other_arr = other.arr();
+#pragma omp parallel for
+    for (size_t i = 0; i < _size; i++)
+    {
+        _arr[i] += other_arr[i];
+    }
+}
